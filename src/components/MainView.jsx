2095a84d1bafd6468a76c4eb2a8999b0e78a98a2
maeng-kim <ehseo0410@gmail.com>
2024-07-30 03:12:35 +0900

    fix: main화면 수정

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "../asset/MainView.scss"
 
 function MainView() {
   return (
-    <div className="container main-view">
-      <h1>PoPo</h1>
-      <Link to="/camera">
-        <button>Start</button>
-      </Link>
+    <div className="main-view-container">
+      <img src='/images/MainGraphicMobile.png' alt='mainImage' className='MainGraphic' />
+      <div className="header">
+        <img src="/images/logo.png" alt="Logo" className="logo" />
+        <Link to="/camera">
+          <button className="start-button">
+            <img src="/images/StartButton.png" alt="next" />
+          </button>
+        </Link>
+      </div>
+      <div className="main-view">
+        <h3><strong>“포포”</strong>랑 사진 찍고 꾸미기</h3>
+      </div>
     </div>
   );
 }
