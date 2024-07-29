cb09cfc5d056d8687d8ceef72390be3317eb5995
maeng-kim <ehseo0410@gmail.com>
2024-07-29 17:30:31 +0900

    fix: 촬영부분 수정

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 function MainView() {
   return (
-    <div className="main-view">
-      <h1>PolPol</h1>
+    <div className="container main-view">
+      <h1>PoPo</h1>
       <Link to="/camera">
         <button>Start</button>
       </Link>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './asset/style.scss';
 
-ReactDOM.render(
+const rootNode = document.getElementById('root');
+
+ReactDOM.createRoot(rootNode).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+
+// );
 //랜더 왜 저따구로 삭제처리되는지 의문 함 봐주쇼요
