ddce587c74ddba422cdbfcd3ad8b9fdd806f4ee6
maeng <89590797+maeng-kim@users.noreply.github.com>
2024-08-06 16:52:18 +0900

    Update CheckView.jsx

--- src/components/CheckView.jsx
+++ src/components/CheckView.jsx
@@ -41,7 +41,7 @@ function CheckView() {
         // 모바일 화면 크기 기준 조정
         let imageWidth, imageHeight, imageX, imageY;
         if (window.innerWidth <= 768) { // 모바일 기기 너비의 예: 768px 이하
-          imageWidth = frameWidth * 0.8; 
+          imageWidth = frameWidth * 0.95;
           imageHeight = frameHeight * 0.65;
           imageX = (frameWidth - imageWidth) / 2.3;
           imageY = (frameHeight - imageHeight) / 2.5;
